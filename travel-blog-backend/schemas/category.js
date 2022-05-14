a7248593ccbb7036802b7c3743a14f3068e4728b
Wandu <31390092+sMiter911@users.noreply.github.com>
2022-05-14 13:52:44 +0200

    Initial Commit

---
+++ travel-blog-backend/schemas/category.js
@@ -0,0 +1,17 @@
+export default {
+  name: "category",
+  type: "document",
+  title: "Category",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "string",
+    },
+  ],
+};
